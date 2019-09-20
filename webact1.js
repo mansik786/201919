df13075eae7cc6d46006d8efe99f79083eb61c79
Mansii <43856199+mansik786@users.noreply.github.com>
2019-09-20 16:50:18 -0400

    Update webact1.js

--- webact1.js
+++ webact1.js
@@ -1,3 +1,7 @@
-<script>
-  <h1> hi </h1>
-</script>
+const sqlite3= require ('sqlite3').verbose()
+
+var Groupname="4 Magicians"
+var name1="Lovepreet (745198)"
+var name2="Mansi (740950)"
+var name3="Maninder(741327)"
+var name4="Devinder (741040)"
